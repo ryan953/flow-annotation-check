e273e7e7ba7b2e562494823f848672fafde1d341
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-01-16 14:57:01 -0800

    Swap out `args` for `argparse` library and rewrite the cli module.

---
+++ src/__tests__/cli-test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const path = require('path');
+const {
+  getParser,
+  main,
+  resolveArgs,
+} = require('../cli');
+
+describe('cli', () => {
+  describe('resolveArgs', () => {
+    const MOCK_ARGS = {
+      root: '.',
+    };
+
+    it('should parse the root into a full path', () => {
+      const result = resolveArgs(MOCK_ARGS);
+      expect(result.root).toMatch(/\//);
+      expect(result).toMatchObject({
+        root: path.resolve(path.join(__dirname, '../..')),
+      });
+    });
+
+    it('should setup defaults for include', () => {
+      const result = resolveArgs(MOCK_ARGS);
+      expect(result).toMatchObject({
+        include: ['**/*.js'],
+      });
+    });
+
+    it('should setup defaults for exclude', () => {
+      const result = resolveArgs(MOCK_ARGS);
+      expect(result).toMatchObject({
+        exclude: ['node_modules/**/*.js'],
+      });
+    });
+
+    it('should let include and exclude pass through when not falsey', () => {
+      const result = resolveArgs({
+        include: ['*.js'],
+        exclude: ['*.coffee'],
+        root: '.',
+      });
+      expect(result).toMatchObject({
+        include: ['*.js'],
+        exclude: ['*.coffee'],
+      });
+      expect(result.root).toMatch(/\//);
+    });
+
+  });
+});
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const args = require('args');
 const path = require('path');
+const packageJSON = require('../package.json');
+
+const {ArgumentParser} = require('argparse');
 
 const {
   getReport,
@@ -10,40 +12,93 @@ const {
   validate,
 } = require('./flow-annotation-check');
 
-function main(command, sub, flags) {
-  if (process.env.VERBOSE) {
-    console.log('Invoked:', {command, sub, flags});
+function getParser() {
+  const parser = new ArgumentParser({
+    addHelp: true,
+    version: packageJSON.version,
+  });
+
+  parser.addArgument(
+    ['-a', '--absolute'],
+    {
+      action: 'storeTrue',
+      help: 'Report absolute path names. (default: false)',
+    },
+  );
+  parser.addArgument(
+    ['-i', '--include'],
+    {
+      action: 'append',
+      help: 'Glob for files to include. Can be set multiple times. (default: `**/*.js`)',
+    },
+  );
+  parser.addArgument(
+    ['-x', '--exclude'],
+    {
+      action: 'append',
+      help: 'Glob for files to exclude. Can be set multiple times. (default: `node_modules/**/*.js`)',
+    },
+  );
+  parser.addArgument(
+    ['--validate'],
+    {
+      action: 'storeTrue',
+      help: 'Run in validation mode. This injects errors into globbed files and checks the flow-annotation status',
+    },
+  );
+  parser.addArgument(
+    ['root'],
+    {
+      defaultValue: '.',
+      help: 'The root directory to glob files from. (default: `.`)',
+      nargs: '?',
+    },
+  );
+
+  return parser;
+}
+
+function resolveArgs(args) {
+  if (!args.include) {
+    args.include = ['**/*.js'];
   }
-  const cwd = path.resolve(sub[0] || '.');
+  if (!args.exclude) {
+    args.exclude = ['node_modules/**/*.js'];
+  }
+  args.root = path.resolve(args.root);
+
+  return args;
+}
+
+function main(args) {
+  const {
+    validate,
+    root,
+    ...flags,
+  } = args;
+
+  const command = validate ? 'validate' : 'report';
+
+  if (process.env.VERBOSE) {
+    console.log('Invoking:', {command, root, flags});
+  }
+
   switch(command) {
     case 'validate':
-      validate(cwd, flags).then(printValidationReport);
+      validate(root, flags).then(printValidationReport);
       break;
     default:
-      console.log('Running report', sub, flags);
-      getReport(cwd, flags).then(printStatusReport);
+      getReport(root, flags).then(printStatusReport);
       break;
   }
 }
 
 module.exports = {
   run() {
-    let mainCommand;
+    main(resolveArgs(getParser().parseArgs()));
+  },
 
-    const setCommand = (command, sub, flags) => {
-      mainCommand = command.shift();
-      main(mainCommand, sub, flags);
-    };
-
-    args
-      .option('absolute', 'Report absolute path names', false)
-      .option(['i', 'include'], 'Glob for files to include', '**/*.js')
-      .option(['x', 'exclude'], 'Glob of files to ignore', 'node_modules/**/*.js')
-      .command('validate', 'Inject errors in files to validate status', setCommand, ['v']);
-    const parsedFlags = args.parse(process.argv);
-    if (!mainCommand) {
-      main(mainCommand, args.sub, parsedFlags);
-    }
-  }
+  getParser,
+  main,
+  resolveArgs,
 };
-
