54a59f56da45024979e87a4e3b79e8644f75ac6c
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-06-10 11:49:36 -0700

    Remove html-annotations shell script, it wasn't working well at all.

--- bin/html-annotations.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-"$(dirname $0)/flow-annotation-check.js" \
-  | awk 'BEGIN{print "<table>"} {print "<tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' \
-  | tee $1
