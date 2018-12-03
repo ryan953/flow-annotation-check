4a3e14de27cc6156d2e024d84570ce7eb849afbc
Ryan Albrecht <ryan@ryanalbrecht.ca>
2018-12-03 00:50:37 -0800

    Add tests for summarizeReport

--- src/flow-annotation-check.js
+++ src/flow-annotation-check.js
@@ -16,25 +16,9 @@ import type {
 
 import globsToFileList from './globsToFileList';
 import isValidFlowStatus from './isValidFlowStatus';
+import summarizeReport from './summarizeReport';
 import {asyncMap} from './promisified';
 import {genCheckFlowStatus, genForceErrors} from './flow';
-
-function countByStatus(files: Array<StatusEntry>, status: FlowStatus): number {
-  return files.filter((entry) => entry.status === status).length;
-}
-
-function summarizeReport(
-  files: Array<StatusEntry>,
-): StatusReportSummary {
-  return {
-    flow: countByStatus(files, 'flow'),
-    flowstrict: countByStatus(files, 'flow strict'),
-    flowstrictlocal: countByStatus(files, 'flow strict-local'),
-    flowweak: countByStatus(files, 'flow weak'),
-    noflow: countByStatus(files, 'no flow'),
-    total: files.length,
-  };
-}
 
 function genSummarizedReport(
   cwd: string,
